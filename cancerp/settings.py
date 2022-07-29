d22b6f1e3332ecb3b3f72c450d3732fd4ce9c85a
Yasser Milato <yasser.milato@gmail.com>
2022-07-29 17:57:18 +0300

    first

--- cancerp/settings.py
+++ cancerp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,11 @@ FRONTEND_DIR = BASE_DIR / 'client'
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vm$d@cll25jctlu1s(&@zadv^2um_(7)t71e3yb!ifxzoa5^f)'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-^kd5+cy8j5b&hw-2z3_^a$y1(v3_d4o-wo-js66g!srgz53mkc')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'caner-detection.herokuapp.com']
 
 # Application definition
 
